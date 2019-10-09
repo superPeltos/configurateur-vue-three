76574bbbc78de8bc3c8a822256387cea7f3c8b96
tpelletier_ <tpelletier@aw-innovate.com>
2019-10-09 18:35:21 +0200

    why all move ?

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 import * as THREE from 'three';
+
 const OrbitControls = require('three-orbitcontrols');
 
-const CAMERA = 1, DRAG = 2;
+const CAMERA = 1, DRAG = 2, ADD = 3;
 
 let roomWidth = 500;
 let roomHeight = 500;
@@ -11,7 +12,7 @@ let mesh;
 let normalMatrix = new THREE.Matrix3();
 let worldNormal = new THREE.Vector3();
 let lookAtVector = new THREE.Vector3();
-
+let objectDragg;
 
 let camera, controls, scene, renderer;
 let mouseAction;
@@ -19,14 +20,17 @@ let dragging = false;
 let raycaster;
 let intersects;
 let targetForDragging;
+let cubes = [];
 
 init();
 //render(); // remove when using next line for animation loop (requestAnimationFrame)
 animate();
+
 function init() {
   mouseAction = CAMERA;
   document.getElementById("mouseCamera").onchange = doChangeMouseAction;
   document.getElementById("mouseDrag").onchange = doChangeMouseAction;
+  document.getElementById("mouseAdd").onchange = doChangeMouseAction;
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xffffff);
@@ -103,21 +107,17 @@ function init() {
     w.position.x = wall.position.x;
     w.position.y = wall.position.y;
     w.position.z = wall.position.z;
-    if(wall.hasOwnProperty('rotation')){
+    if (wall.hasOwnProperty('rotation')) {
       w.rotation.y = wall.rotation.y
     }
     scene.add(w);
   });
 
   // Object
-  geometry = new THREE.BoxGeometry(25, 25, 25);
-  material = new THREE.MeshPhongMaterial({color: 0xffffff, flatShading: true});
-  mesh = new THREE.Mesh(geometry, material);
-  mesh.position.x = 0;
-  mesh.position.y = mesh.geometry.parameters.height / 2;
-  mesh.position.z = 0;
-
-  scene.add(mesh);
+  generateMesh(0,0);
+  cubes.forEach((cube) => {
+    scene.add(cube);
+  });
 
   // Lights
   var light = new THREE.DirectionalLight(0xffffff);
@@ -132,44 +132,76 @@ function init() {
   window.addEventListener('resize', onWindowResize, false);
 
   targetForDragging = new THREE.Mesh(
-    new THREE.BoxGeometry(100,0.01,100),
+    new THREE.BoxGeometry(100, 0.01, 100),
     new THREE.MeshBasicMaterial()
   );
   targetForDragging.material.visible = false;
 
   raycaster = new THREE.Raycaster();
 }
+
+
+
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
+
 function animate() {
-  requestAnimationFrame( animate );
-  if(mouseAction!==CAMERA) {
+  requestAnimationFrame(animate);
+  if (mouseAction !== CAMERA) {
     controls.enabled = false;
-  }
-  else {
+  } else {
     controls.enabled = true;
   }
   render();
 }
+
 function render() {
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 
 function doChangeMouseAction() {
+
   if (document.getElementById("mouseCamera").checked) {
     mouseAction = CAMERA;
-  }
-  else if (document.getElementById("mouseDrag").checked) {
+  } else if (document.getElementById("mouseDrag").checked) {
     mouseAction = DRAG;
+  } else if (document.getElementById("mouseAdd").checked) {
+    mouseAction = ADD;
   }
 }
 
 
-function doMouseDown(x,y){
-  dragging = true;
+function doMouseDown(event) {
+  if (mouseAction === DRAG) {
+    dragging = true;
+    /*mouse.set((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1);
+    raycaster.setFromCamera(mouse, camera);
+    intersects = raycaster.intersectObjects(scene.children);
+    if (intersects.length === 0 ) return;
+    objectDragg = intersects[0].object;*/
+  } else if (mouseAction === ADD) {
+    console.log('coucou');
+    mouse.set((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1);
+    raycaster.setFromCamera(mouse, camera);
+    intersects = raycaster.intersectObjects(scene.children);
+    if (intersects.length === 0 ) return;
+
+    normalMatrix.getNormalMatrix(intersects[0].object.matrixWorld);
+    worldNormal.copy(intersects[0].face.normal).applyMatrix3(normalMatrix).normalize();
+    //let newPos = intersects[0].point;
+    console.log(cubes);
+    generateMesh();
+    cubes.forEach((cube) => {
+      scene.add(cube);
+    });
+
+//    let a = Math.min(roomWidth / 2 - mesh.geometry.parameters.width / 2, Math.max(-roomWidth / 2 + mesh.geometry.parameters.width / 2, newPos.x));  // clamp coords to the range -19 to 19, so object stays on ground
+//    let b = Math.min(roomWidth / 2 - mesh.geometry.parameters.width / 2, Math.max(-roomWidth / 2 + mesh.geometry.parameters.width / 2, newPos.z));
+//    mesh.position.set(a, mesh.position.y, b);
+  }
 //
 //  if(mouseAction!==CAMERA) {
 //    var a = 2*x/renderer.domElement.width - 1;
@@ -190,30 +222,39 @@ function doMouseDown(x,y){
 }
 
 function doMouseMove(event) {
-  if(mouseAction===CAMERA) {
+  if (mouseAction === CAMERA || mouseAction === ADD) {
     return true;
-  }
-  else {
-    if(dragging){
+  } else {
+    if (dragging) {
       mouse.set((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1);
       raycaster.setFromCamera(mouse, camera);
       intersects = raycaster.intersectObjects(scene.children);
       if (intersects.length === 0 || !dragging) return;
 
-      console.log(intersects);
-
       normalMatrix.getNormalMatrix(intersects[0].object.matrixWorld);
       worldNormal.copy(intersects[0].face.normal).applyMatrix3(normalMatrix).normalize();
       let newPos = intersects[0].point;
 
-      let a = Math.min(roomWidth/2-mesh.geometry.parameters.width/2,Math.max(-roomWidth/2+mesh.geometry.parameters.width/2,newPos.x));  // clamp coords to the range -19 to 19, so object stays on ground
-      let b = Math.min(roomWidth/2-mesh.geometry.parameters.width/2,Math.max(-roomWidth/2+mesh.geometry.parameters.width/2,newPos.z));
-      mesh.position.set(a,mesh.position.y,b);
+      let a = Math.min(roomWidth / 2 - mesh.geometry.parameters.width / 2, Math.max(-roomWidth / 2 + mesh.geometry.parameters.width / 2, newPos.x));  // clamp coords to the range -19 to 19, so object stays on ground
+      let b = Math.min(roomWidth / 2 - mesh.geometry.parameters.width / 2, Math.max(-roomWidth / 2 + mesh.geometry.parameters.width / 2, newPos.z));
+      objectDragg = intersects[0].object;
+      objectDragg.position.set(a, mesh.position.y, b);
 
-      render();
+
     }
   }
 }
+
 function doMouseUp() {
   dragging = false;
+}
+
+function generateMesh(x,z){
+  let geometry = new THREE.BoxGeometry(25, 25, 25);
+  let material = new THREE.MeshPhongMaterial({color: 0xffffff, flatShading: true});
+  mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = 0;
+  mesh.position.y = mesh.geometry.parameters.height / 2;
+  mesh.position.z = 0;
+  cubes.push(mesh);
 }
